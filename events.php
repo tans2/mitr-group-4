7138eb5453a6a100bdc095ee5862491607f482cc
jtgjohn <gayj@rpi.edu>
2018-10-26 02:01:57 -0400

    Created attendance table

---
+++ events.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+
+<html>
+<head>
+	<title>Cadet Events</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+</head>
+
+<body>
+	<p>
+		Select Event
+		<select name="eventSelect">
+		</select>
+	</p>
+</body>
+</html>
