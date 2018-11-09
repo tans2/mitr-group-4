647a6bc34d984fbf64203d3a02943f1d953b44bc
jtgjohn <gayj@rpi.edu>
2018-11-09 12:01:22 -0500

    attendance feature

--- events.php
+++ events.php
@@ -10,8 +10,18 @@
 <body>
 	<p>
 		Select Event
-		<select name="eventSelect">
-		</select>
+		<form action="attend.php" method="post">
+			<select name="eventSelect">
+				<?php
+				@ $db =  new mysqli('localhost', 'root', 'password', 'mitr');
+				$result = $db->query("SELECT name, eventID FROM cadetevent");
+				while($row = $result->fetch_assoc()) {
+					echo '<option value="' . $row['eventID'] . '">'.$row['name'] . '</option>';
+				}
+				?>
+			</select>
+			<input type="submit" id="selectevent" value="Submit"/>
+		</form>
 	</p>
 </body>
 </html>
