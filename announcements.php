f341bfd33d3a2d4e39f97a28db606d050bbc5373
jtgjohn <gayj@rpi.edu>
2018-11-26 11:39:14 -0500

    worked on announcements

--- announcements.php
+++ announcements.php
@@ -1,4 +1,10 @@
-<?php include('./assets/inc/header.php'); ?>
+<?php
+include('./assets/inc/header.php');
+if ( !isset($_SESSION['login']) || !$_SESSION['login'] )
+{
+    header('Location: index.php');
+}
+?>
 
 <body>
 
@@ -9,6 +15,19 @@
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+	<?php
+	$query = 'SELECT * FROM announcement ORDER BY posted_on DESC';
+	$result = $mysqli->query($query);
+	while ($row = $result->fetch_assoc()) {
+		echo $row['title'];
+		echo $row['subject'];
+		echo $row['body'];
+		$poster = $mysqli->query('SELECT firstName, lastName FROM cadet WHERE rin="' $row['rin'] . '"');
+		$names = $poster->fetch_assoc();
+		echo $names['firstName'] . ' ' . $names['lastName'];
+	}
+	?>
 
 </body>
 
