ce48e74d09c150cbfbe60ebb18fb44b160770b07
jtgjohn <gayj@rpi.edu>
2018-11-26 16:34:21 -0500

    should fix my problem

--- assets/inc/header.php
+++ assets/inc/header.php
@@ -1,17 +1,22 @@
 <?php
 $host = "localhost";
 $user = "root";
-$password = "";
+$password = "password";
 $database = "mitr";
-
 // Create connection
 $mysqli = mysqli_connect($host, $user, $password, $database);
-
 // Check connection
 if ($mysqli->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-// echo "Connected successfully";
+//echo "Connected successfully";
+session_start();
+require_once('./assets/cadet.php');
+// Checks to see if user is already logged in
+if ( isset($_SESSION['login']) && $_SESSION['login'] )
+{
+    $cadet = new cadet( $_SESSION["rin"], $mysqli );
+}
 ?>
 
 <!DOCTYPE html>
@@ -19,10 +24,12 @@ if ($mysqli->connect_error) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Profile</title>
+    <title>Home</title>
     <link type="text/css" rel="stylesheet" href="indexStylesheet.css">
     <link href='https://fonts.googleapis.com/css?family=Cabin' rel='stylesheet'>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -32,28 +39,43 @@ if ($mysqli->connect_error) {
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item active">
             <a class="nav-link" href="home.php">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="profile.php">Profile</a>
+            <a class="nav-link" href="myprofile.php">Profile</a>
           </li>
           <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              More
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> More
             </a>
             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
               <a class="dropdown-item" href="attendance.php">Attendance</a>
               <a class="dropdown-item" href="announcements.php">Announcements</a>
-              <a class="dropdown-item" href="#">Media/Documents</a>
-              <a class="dropdown-item" href="logout.php">Logout</a>
+                <a class="dropdown-item" href="directory.php">Cadet Directory</a>
+              <a class="dropdown-item" href="https://rpi.account.box.com/login">Media/Documents</a>
+        <?php 
+          if(isset($_SESSION["rin"])){
+                    $sql = "SELECT admin FROM cadet WHERE rin = (?)";
+                    $stmt = $mysqli->prepare($sql);
+                    if(!($stmt->bind_param( "i", $_SESSION["rin"] )))
+                    {
+                        echo "Prepared statement bind failed!";
+                    }
+                    $stmt->execute();
+                    $result = $stmt->get_result();
+                    $row = $result->fetch_assoc();
+              
+              if($row['admin'] === 1) {
+                  echo "<a class=\"dropdown-item\" href=\"admin.php\">Admin Page</a>";
+              }
+        }
+                ?>
             </div>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="home.php">Log Out</a>
+            <a class="nav-link" href="logout.php">Log Out</a>
           </li>
         </ul>
       </div>
