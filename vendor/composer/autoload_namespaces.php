f6ae5e6142672dca51020865e65b36f4e1ac8d05
Andrew Son <sonj2@rpi.edu>
2018-11-27 18:00:42 -0500

    Remove uneeded dependencies

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -6,8 +6,4 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'PEAR' => array($vendorDir . '/pear/pear_exception'),
-    'Mail' => array($vendorDir . '/pear/mail'),
-    'Console' => array($vendorDir . '/pear/console_getopt'),
-    '' => array($vendorDir . '/pear/pear-core-minimal/src'),
 );
