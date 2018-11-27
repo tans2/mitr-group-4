b8739372c1a7fcb393faed40a8caa312b2e0c9f8
Stephanie Tan <tans2@rpi.edu>
2018-11-27 18:35:06 -0500

    Profile Page Fixes

--- viewprofile.php
+++ viewprofile.php
@@ -9,40 +9,76 @@ $cadet = new cadet( $_GET['rin'], $mysqli );
 ?>
 
 
-<div class="jumbotron">
-    <h1 class="display-4"><?php echo "Cadet " . $cadet->getLast() ?></h1>
-    <div style="display:block; height: 300px">
-        <img class="picture" src=<?php
-                $files = glob("./assets/images/*.{jpg,png,jpeg}", GLOB_BRACE);
-                $found = false;
-                foreach($files as $file)
+<body>
+  <div class="jumbotron">
+      <h1 class="display-4"><?php echo "Cadet " . $cadet->getLast() ?></h1>
+      <div class="card" style="width:25rem;">
+        <img class="card-img-top" alt="Profile picture" src=
+        <?php
+          $files = glob("./assets/images/*.{jpg,png,jpeg}", GLOB_BRACE);
+          $found = false;
+          foreach($files as $file)
+            {
+              $info = pathinfo($file);
+              if($info['filename'] === $_SESSION['rin'])
                 {
-                    $info = pathinfo($file);
-                    if($info['filename'] === $_GET['rin'])
-                    {
-                        echo $file; 
-                        $found = true;
-                    }
+                  echo $file; 
+                  $found = true;
                 }
-                if(!$found)
-                {
-                    echo "assets/images/default.jpeg";
-                }
-             ?> height="300px" width="300px" alt="Profile picture"/>
-    </div><br>
-    <p class="lead">
-    <strong>Contact Information: </strong><br>
-    <strong>Email: </strong> <?php echo $cadet->getPrimEmail() ?><br>
-    <strong>Phone Number: </strong> <?php echo $cadet->getPrimPhone() ?><br>
-    <strong>Flight: </strong> <?php echo $cadet->getFlight() ?><br>
-    <strong>Position: </strong> <?php echo $cadet->getPosition() ?><br>
-  </p>
-    <hr class="my-4">
-        <p><strong>Rank:</strong> <?php echo $cadet->getRank() ?></p>
-    <p><strong>Bio: </strong><?php echo $cadet->getBio() ?></p>
-    <p><strong>Awards and Achievements: </strong><?php echo $cadet->getAwards() ?></p>
-    <p><strong>Air Force Goals: </strong><?php echo $cadet->getAirForceGoals() ?></p>
-    <p><strong>Personal Goals: </strong><?php echo $cadet->getPersonalGoals() ?></p>
-</div>
+            }
+              if(!$found)
+              {
+                echo "assets/images/default.jpeg";
+              }
+        ?> >
+
+        <div class="card-body">
+            <p class="card-text">
+            <strong>Contact Information: </strong><br>
+            <strong>Email: </strong> <?php echo $cadet->getPrimEmail() ?><br>
+            <strong>Phone Number: </strong> <?php echo $cadet->getPrimPhone() ?><br>
+            <strong>Flight: </strong> <?php echo $cadet->getFlight() ?><br>
+            <strong>Position: </strong> <?php echo $cadet->getPosition() ?><br>
+            </p>
+        </div>
+      </div>
+
+
+      <div class="card">
+        <div class="card-body">
+            <h5 class="card-title">Rank:</h5>
+            <p class="card-text"><?php echo $cadet->getRank() ?></p>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="card-body">
+            <h5 class="card-title">Bio:</h5>
+            <p class="card-text"><?php echo $cadet->getBio() ?></p>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="card-body">
+            <h5 class="card-title">Awards and Achievements:</h5>
+            <p class="card-text"><?php echo $cadet->getAwards() ?></p>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="card-body">
+            <h5 class="card-title">Air Force Goals:</h5>
+            <p class="card-text"><?php echo $cadet->getAirForceGoals() ?></p>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="card-body">
+            <h5 class="card-title">Personal Goals:</h5>
+            <p class="card-text"><?php echo $cadet->getPersonalGoals() ?></p>
+        </div>
+      </div>
+  </div>
+</body>
     
 <?php include('./assets/inc/footer.php') ?>
