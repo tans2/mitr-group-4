1f025dec96fcad8a87b123965070865b317a13a1
jmessare46 <jmessare46@gmail.com>
2018-12-02 19:30:22 -0500

    Switched wiki to ajax requests

--- assets/wiki/savewiki.php
+++ assets/wiki/savewiki.php
@@ -2,73 +2,73 @@
 include("../objects/wiki.php");
 include("../inc/dbinfo.php");
 
-if(isset($_GET['index']))
+if(isset($_POST['index']))
 {
     $wiki = new wiki( "index", $mysqli );
-    $wiki->setBody($_GET['index']);
+    $wiki->setBody($_POST['index']);
     $wiki->updateWiki($mysqli);
     header("Location: indexwiki.php");
 } 
-elseif(isset($_GET['profile'])) 
+elseif(isset($_POST['profile'])) 
 {
     $wiki = new wiki( "profile", $mysqli );
-    $wiki->setBody($_GET['profile']);
+    $wiki->setBody($_POST['profile']);
     $wiki->updateWiki($mysqli);
     header("Location: profilewiki.php");
 }
-elseif(isset($_GET['home']))
+elseif(isset($_POST['home']))
 {
     $wiki = new wiki( "home", $mysqli );
-    $wiki->setBody($_GET['home']);
+    $wiki->setBody($_POST['home']);
     $wiki->updateWiki($mysqli);
     header("Location: homewiki.php");
 }
-elseif(isset($_GET['editprofile']))
+elseif(isset($_POST['editprofile']))
 {
     $wiki = new wiki( "editprofile", $mysqli );
-    $wiki->setBody($_GET['editprofile']);
+    $wiki->setBody($_POST['editprofile']);
     $wiki->updateWiki($mysqli);
     header("Location: editprofilewiki.php");
 }
-elseif(isset($_GET['announcements']))
+elseif(isset($_POST['announcements']))
 {
     $wiki = new wiki( "announcements", $mysqli );
-    $wiki->setBody($_GET['announcements']);
+    $wiki->setBody($_POST['announcements']);
     $wiki->updateWiki($mysqli);
     header("Location: announcementswiki.php");
 }
-elseif(isset($_GET['events']))
+elseif(isset($_POST['events']))
 {
     $wiki = new wiki( "events", $mysqli );
-    $wiki->setBody($_GET['events']);
+    $wiki->setBody($_POST['events']);
     $wiki->updateWiki($mysqli);
     header("Location: eventswiki.php");
 }
-elseif(isset($_GET['email']))
+elseif(isset($_POST['email']))
 {
     $wiki = new wiki( "email", $mysqli );
-    $wiki->setBody($_GET['email']);
+    $wiki->setBody($_POST['email']);
     $wiki->updateWiki($mysqli);
     header("Location: emailwiki.php");
 }
-elseif(isset($_GET['directory']))
+elseif(isset($_POST['directory']))
 {
     $wiki = new wiki( "directory", $mysqli );
-    $wiki->setBody($_GET['directory']);
+    $wiki->setBody($_POST['directory']);
     $wiki->updateWiki($mysqli);
     header("Location: directorywiki.php");
 }
-elseif(isset($_GET['admin']))
+elseif(isset($_POST['admin']))
 {
     $wiki = new wiki( "admin", $mysqli );
-    $wiki->setBody($_GET['admin']);
+    $wiki->setBody($_POST['admin']);
     $wiki->updateWiki($mysqli);
     header("Location: adminwiki.php");
 }
-elseif(isset($_GET['faq']))
+elseif(isset($_POST['faq']))
 {
     $wiki = new wiki( "faq", $mysqli );
-    $wiki->setBody($_GET['faq']);
+    $wiki->setBody($_POST['faq']);
     $wiki->updateWiki($mysqli);
     header("Location: faqwiki.php");
 }
