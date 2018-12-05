f595d742c5be0b297e5284f789e85e53caec6113
jmessare46 <jmessare46@gmail.com>
2018-12-05 06:32:36 -0500

    Fixed login page resizing

--- index.php
+++ index.php
@@ -9,6 +9,19 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    
+    <style>
+@media (max-width: 500px) {
+    div.card {
+        width: 100%;
+    }
+}
+        @media (min-width: 500px) {
+    div.card {
+        width: 40%;
+    }
+}
+</style>
 </head>
     
     <?php 
@@ -19,7 +32,7 @@ if ( isset($_SESSION['login']) && $_SESSION['login'] )
 ?>
 
 <body class="text-center"> 
-<div class="card" style="margin: auto;width: 30%;padding: 10px;">
+<div class="card" style="margin: auto;padding: 10px;">
   <div class="card-body">
   	<form id="login" method="POST" action="loginAuth.php">
       <img class="img-fluid" src="assets/images/default.jpeg" alt="Responsive image">
