ba9a8ddfedf4c37591df029e8d51fd9cb8ce60c5
jmessare46 <jmessare46@gmail.com>
2018-12-05 17:57:00 -0500

    Added time stamp to announcements

--- assets/sql/mitr.sql
+++ assets/sql/mitr.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Nov 30, 2018 at 05:51 AM
+-- Generation Time: Dec 05, 2018 at 11:37 PM
 -- Server version: 10.1.36-MariaDB
 -- PHP Version: 7.2.11
 
@@ -25,28 +25,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `announcement`
---
-
-CREATE TABLE `announcement` (
-  `title` varchar(255) COLLATE ascii_bin NOT NULL,
-  `subject` varchar(255) COLLATE ascii_bin NOT NULL,
-  `body` mediumtext COLLATE ascii_bin NOT NULL,
-  `createdBy` int(10) NOT NULL,
-  `uid` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=ascii COLLATE=ascii_bin;
-
---
--- Dumping data for table `announcement`
---
-
-INSERT INTO `announcement` (`title`, `subject`, `body`, `createdBy`, `uid`) VALUES
-('Fake Announcement', 'This is a test.', 'This is a fake announcement with random text in it about nothing very important.', 123123123, 1),
-('Fake Announcement', 'Something', 'Something happened that\'s important', 123123123, 2);
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `acknowledge_posts`
 --
 
@@ -56,15 +34,39 @@ CREATE TABLE `acknowledge_posts` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Indexes for dumped tables
+-- Dumping data for table `acknowledge_posts`
 --
 
+INSERT INTO `acknowledge_posts` (`rin`, `announcement_id`) VALUES
+(123123123, 1),
+(123123123, 2),
+(123123123, 3);
+
+-- --------------------------------------------------------
+
 --
--- Indexes for table `acknowledge_posts`
+-- Table structure for table `announcement`
 --
-ALTER TABLE `acknowledge_posts`
-  ADD PRIMARY KEY (`rin`,`announcement_id`);
-  
+
+CREATE TABLE `announcement` (
+  `title` varchar(255) COLLATE ascii_bin NOT NULL,
+  `subject` varchar(255) COLLATE ascii_bin NOT NULL,
+  `body` mediumtext COLLATE ascii_bin NOT NULL,
+  `createdBy` int(10) NOT NULL,
+  `uid` int(11) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=ascii COLLATE=ascii_bin;
+
+--
+-- Dumping data for table `announcement`
+--
+
+INSERT INTO `announcement` (`title`, `subject`, `body`, `createdBy`, `uid`, `date`) VALUES
+('Fake Announcement', 'This is a test.', 'This is a fake announcement with random text in it about nothing very important.', 123123123, 1, '2018-12-05 22:34:55'),
+('Fake Announcement', 'Something', 'Something happened that\'s important', 123123123, 2, '2018-12-05 22:34:55'),
+('New E', 'New Event', 'asldkfjalsdkjf', 123123123, 3, '2018-12-05 22:34:55'),
+('asldkfjasdlkjf', 'ads;lfkj asdfja;d', 'as;ldk fjas;dlkfj a;sldk jf;qlkj ', 123123123, 4, '2018-12-05 22:35:16');
+
 -- --------------------------------------------------------
 
 --
@@ -83,9 +85,10 @@ CREATE TABLE `attendance` (
 --
 
 INSERT INTO `attendance` (`rin`, `eventid`, `excused_absence`, `present`) VALUES
+(123123122, 1, NULL, 0),
 (123123123, 1, NULL, 0),
-(123123123, 4294967295, 0, 0),
-(661550966, 4, 1, 0);
+(123123123, 4, NULL, 0),
+(123123123, 4294967295, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -119,9 +122,9 @@ CREATE TABLE `cadet` (
 --
 
 INSERT INTO `cadet` (`firstName`, `rank`, `rin`, `primaryEmail`, `secondaryEmail`, `primaryPhone`, `secondaryPhone`, `password`, `bio`, `flight`, `position`, `groupMe`, `AFGoals`, `awards`, `lastName`, `PGoals`, `admin`, `rfid`) VALUES
-('', 'AS100', 0, NULL, NULL, 0, NULL, '$2y$10$ZD58kTG9d8A3XPwBqM2KKOKF6HtUsK9TTSvZscRh7p9L4ZcC4loAm', NULL, 'Alpha', NULL, '', NULL, NULL, '', '', 1, 0),
-('John', 'AS100', 123123123, NULL, NULL, 0, NULL, '$2y$10$67.c/sLiPKRPsJCebeCoM.pXokIK40WicOf4.YZh7BN9a1jS/gbvm', NULL, 'Alpha', NULL, '', NULL, NULL, 'Doe', '', 1, 0),
-('Joseph', 'AS300', 661550966, 'faek', '', 0, 0, '$2y$10$dDvXazh5p65sFCDC0vxRZuqdy9iXeQ1eux2dKrfDOrn7kP0K9taAW', NULL, 'Alpha', '', '', NULL, NULL, 'Messare', '', 1, 22983);
+('Andrew', 'AS100', 111111111, NULL, NULL, 0, NULL, '$2y$10$scfVYXBcjYPuYSjMr815jebW0.etWyyqDqH0kkBBL66akdfPFQ5D.', NULL, 'None', NULL, '', NULL, NULL, 'Messare', '', 0, 0),
+('new ', 'AS200', 123123122, NULL, NULL, 0, NULL, '$2y$10$4DiL2ue.CkmJSzq9Jy6T5eN2aPtLAsT19eGYZn6FcgHbcfL.XBaOu', NULL, 'None', NULL, '', NULL, NULL, 'user', '', 0, 0),
+('John', 'None', 123123123, 'jmessare46@gmail.com', 'jmessare46@gmail.com', 5186053642, 0, '$2y$10$67.c/sLiPKRPsJCebeCoM.pXokIK40WicOf4.YZh7BN9a1jS/gbvm', '<p>This is my new bio</p><p>sdf</p><p><br></p><p><br></p>', 'None', '', '', '<p>asdfasd</p>', '<p>fff</p>', 'Doe', '<p>asdfasd</p>', 1, 0);
 
 -- --------------------------------------------------------
 
@@ -144,7 +147,8 @@ INSERT INTO `cadetEvent` (`name`, `mandatory`, `date`, `eventID`) VALUES
 ('Test Event', 1, '2018-11-22 00:00:00', 1),
 ('Fake Event', 1, '2018-11-29 00:00:00', 2),
 ('askdjfaskd', 1, '2018-11-29 00:00:00', 3),
-('LLAB', 1, '2018-11-29 00:00:00', 4);
+('LLAB', 1, '2018-11-29 00:00:00', 4),
+('PT 37', 1, '2018-12-01 00:00:00', 5);
 
 -- --------------------------------------------------------
 
@@ -162,7 +166,8 @@ CREATE TABLE `cadetGroup` (
 --
 
 INSERT INTO `cadetGroup` (`label`, `id`) VALUES
-('Operations Group', 1);
+('Operations Group', 1),
+('New Fake Group', 2);
 
 -- --------------------------------------------------------
 
@@ -180,7 +185,9 @@ CREATE TABLE `groupMember` (
 --
 
 INSERT INTO `groupMember` (`groupID`, `rin`) VALUES
-(1, 123123123);
+(1, 111111111),
+(1, 123123123),
+(2, 123123123);
 
 -- --------------------------------------------------------
 
@@ -198,21 +205,27 @@ CREATE TABLE `wiki` (
 --
 
 INSERT INTO `wiki` (`name`, `body`) VALUES
-('admin', ''),
+('admin', '<p>asdfas</p>'),
 ('announcements', '<p>Announcements Wiki Page</p>'),
 ('attendance', ''),
 ('directory', '<p>Directory Wiki Page</p>'),
-('editprofile', '<p>Edit Profile Wiki'),
+('editprofile', '<p>Edit Profile Wiki</p>'),
 ('email', '<p>Email Wiki Page</p>'),
 ('events', '<p>Events Wiki Page</p>'),
 ('faq', '<h1><b><u>FAQ</u></b></h1>'),
-('home', '<p><br></p><p><br></p><p>asdfasdf</p><p><br></p><p>asdfasdfasdf<p></p></p>'),
-('index', '<h3>This is a test</h3><p>Not sure</p><p>asdfas</p>'),
+('home', '<h3>This is the new Wiki Page<br>as</h3><div class=\"card-header\" id=\"headingThree\">\n      		<h5 class=\"mb-0\">\n        	<button class=\"btn btn-link collapsed\" type=\"button\" data-toggle=\"collapse\" data-target=\"#collapseThree\" aria-expanded=\"false\" aria-controls=\"collapseThree\">\n          	  Profile Page\n        	</button>\n      		</h5>\n    	  </div>\n    	  <div id=\"collapseThree\" class=\"in collapse\" aria-labelledby=\"headingThree\" data-parent=\"#docs\" style=\"\" aria-expanded=\"true\">\n      	    <div class=\"card-body\">\n      	  	  <button id=\"edit\" class=\"btn btn-primary btn-sm\" onclick=\"editProfile()\" type=\"button\">Edit</button>\n			  <button id=\"save\" class=\"btn btn-primary btn-sm\" onclick=\"saveProfile()\" type=\"button\">Save</button><br>\n			  <div class=\"profilewiki\" style=\"\"><h1><b><u>Profile Wiki Page</u></b></h1></div> \n      	    </div>\n    	  </div>\n  		'),
+('index', '<p>sss</p>'),
 ('profile', '<h1><b><u>Profile Wiki Page</u></b></h1>');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `acknowledge_posts`
+--
+ALTER TABLE `acknowledge_posts`
+  ADD PRIMARY KEY (`rin`,`announcement_id`);
 
 --
 -- Indexes for table `announcement`
@@ -245,6 +258,12 @@ ALTER TABLE `cadetGroup`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `groupMember`
+--
+ALTER TABLE `groupMember`
+  ADD UNIQUE KEY `UQ_EmpID_DeptID` (`groupID`,`rin`);
+
+--
 -- Indexes for table `wiki`
 --
 ALTER TABLE `wiki`
@@ -258,19 +277,19 @@ ALTER TABLE `wiki`
 -- AUTO_INCREMENT for table `announcement`
 --
 ALTER TABLE `announcement`
-  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `cadetEvent`
 --
 ALTER TABLE `cadetEvent`
-  MODIFY `eventID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `eventID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `cadetGroup`
 --
 ALTER TABLE `cadetGroup`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
