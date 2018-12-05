eaac40cc094dcd69033775ae7492ca5d93f90fac
Stephanie Tan <tans2@rpi.edu>
2018-12-05 15:42:43 -0500

    Edit Profile Responsiveness

--- editprofile.php
+++ editprofile.php
@@ -17,30 +17,30 @@ if ( !isset($_SESSION['login']) || !$_SESSION['login'] )
             <p><strong>Profile Picture: </strong></p><input type="file" placeholder="Enter name here" name="file"><br><br>
             <div>
                 <strong>Primary Email:</strong><br>
-                <input id="pemail" type="text" name="pemail" size="30" value="<?php echo $cadet->getPrimEmail() ?>"/>
+                <input class="form-control" id="pemail" type="text" name="pemail" size="30" value="<?php echo $cadet->getPrimEmail() ?>"/>
             </div><br>
             <div>
                 <strong>Secondary Email:</strong><br>
-                <input type="text" name="semail" size="30" value="<?php echo $cadet->getSecEmail() ?>"/>
+                <input class="form-control" type="text" name="semail" size="30" value="<?php echo $cadet->getSecEmail() ?>"/>
             </div><br>
             <div>
                 <strong>Primary Phone:</strong><br>
-                <input id="pphone" type="text" name="pphone" size="30" value="<?php echo $cadet->getPrimPhone() ?>"/>
+                <input class="form-control" id="pphone" type="text" name="pphone" size="30" value="<?php echo $cadet->getPrimPhone() ?>"/>
             </div><br>
             <div>
                 <strong>Secondary Phone:</strong><br>
-                <input type="text" name="sphone" size="30" value="<?php echo $cadet->getSecPhone() ?>"/>
+                <input class="form-control" type="text" name="sphone" size="30" value="<?php echo $cadet->getSecPhone() ?>"/>
             </div><br>
             <div>
                 <strong>GroupMe:</strong><br>
-                <input type="text" name="groupme" size="30" value="<?php echo $cadet->getGroupMe() ?>"/>
+                <input class="form-control" type="text" name="groupme" size="30" value="<?php echo $cadet->getGroupMe() ?>"/>
             </div><br>
             <div>
                 <strong>Position:</strong><br>
-                <input type="text" name="position" size="30" value="<?php echo $cadet->getPosition() ?>"/>
+                <input class="form-control"type="text" name="position" size="30" value="<?php echo $cadet->getPosition() ?>"/>
             </div><br>
-            <button class="btn btn-primary" type="reset">Reset</button>
-            <button class="btn btn-primary" type="submit" name="submit">Save Changes</button>
+            <button class="btn btn-sm btn-primary" type="reset">Reset</button>
+            <button class="btn btn-sm btn-primary" type="submit" name="submit">Save Changes</button>
         </form><br>
       </div>
     </div>
@@ -51,23 +51,23 @@ if ( !isset($_SESSION['login']) || !$_SESSION['login'] )
         <div class="card-body">
           <strong>Bio: </strong>
           <div class="cadetBio"><?php echo $cadet->getBio() ?></div>
-          <button id="edit" class="btn btn-primary" onclick="editBio()" type="button">Edit</button>
-          <button id="save" class="btn btn-primary" onclick="saveBio()" type="button">Save</button><br><br>
+          <button id="edit" class="btn btn-sm btn-primary" onclick="editBio()" type="button">Edit</button>
+          <button id="save" class="btn btn-sm btn-primary" onclick="saveBio()" type="button">Save</button><br><br>
 
           <strong>Air Force Goals: </strong>
           <div class="afGoals"><?php echo $cadet->getAirForceGoals() ?></div>
-          <button id="edit" class="btn btn-primary" onclick="editAFG()" type="button">Edit</button>
-          <button id="save" class="btn btn-primary" onclick="saveAFG()" type="button">Save</button><br><br>
+          <button id="edit" class="btn btn-sm btn-primary" onclick="editAFG()" type="button">Edit</button>
+          <button id="save" class="btn btn-sm btn-primary" onclick="saveAFG()" type="button">Save</button><br><br>
 
           <strong>Personal Goals: </strong>
           <div class="pGoals"><?php echo $cadet->getPersonalGoals() ?></div>
-          <button id="edit" class="btn btn-primary" onclick="editPG()" type="button">Edit</button>
-          <button id="save" class="btn btn-primary" onclick="savePG()" type="button">Save</button><br><br>
+          <button id="edit" class="btn btn-sm btn-primary" onclick="editPG()" type="button">Edit</button>
+          <button id="save" class="btn btn-sm btn-primary" onclick="savePG()" type="button">Save</button><br><br>
 
           <strong>Awards and Achievements: </strong>
           <div class="awards"><?php echo $cadet->getAwards() ?></div>
-          <button id="edit" class="btn btn-primary" onclick="editAA()" type="button">Edit</button>
-          <button id="save" class="btn btn-primary" onclick="saveAA()" type="button">Save</button><br><br>
+          <button id="edit" class="btn btn-sm btn-primary" onclick="editAA()" type="button">Edit</button>
+          <button id="save" class="btn btn-sm btn-primary" onclick="saveAA()" type="button">Save</button><br><br>
         </div>
       </div>
     </div>
@@ -80,18 +80,18 @@ if ( !isset($_SESSION['login']) || !$_SESSION['login'] )
             <h5 class="card-title">Change Password</h5>
             <div>
                 <strong>Old Password:</strong><br>
-                <input type="password" name="oldPass" id="oldPass" size="30"/>
+                <input class="form-control" type="password" name="oldPass" id="oldPass" size="30"/>
             </div><br>
             <div>
                 <strong>New Password:</strong><br>
-                <input type="text" name="newPass" id="newPass" size="30"/>
+                <input class="form-control" type="text" name="newPass" id="newPass" size="30"/>
             </div><br>
             <div>
                 <strong>Verify Password:</strong><br>
-                <input id="verPass" type="text" name="verPass" size="30"/>
+                <input class="form-control" id="verPass" type="text" name="verPass" size="30"/>
             </div><br>
-            <button class="btn btn-primary" type="reset">Reset</button>
-            <button class="btn btn-primary" type="submit" name="updatepass">Change Password</button>
+            <button class="btn btn-sm btn-primary" type="reset">Reset</button>
+            <button class="btn btn-sm btn-primary" type="submit" name="updatepass">Change Password</button>
           </form><br>
         </div>
       </div>
