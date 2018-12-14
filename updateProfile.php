d78f7d21eee3163d0830fe3fb0a8302a5fac75c8
jmessare46 <jmessare46@gmail.com>
2018-12-14 18:04:09 -0500

    Added more info to filesize alert

--- updateProfile.php
+++ updateProfile.php
@@ -8,22 +8,8 @@ if ( !isset($_SESSION['login']) || !$_SESSION['login'] )
 
     if(isset($_POST['submit']))
     {
+        $uploadOK = 1;
         saveGenInfo( $cadet, $mysqli, $_POST['pphone'], $_POST['sphone'], $_POST['pemail'], $_POST['semail'], $_POST['groupme'], $_POST['position'], $_POST['major'] );
-    }
-
-    if(isset($_FILES['file']))
-    {
-        $target_dir = "./assets/images/";        
-        $ext = ".jpg";
-        $target_file = $target_dir . $_SESSION['rin'] . $ext;
-        $temp_file = $_FILES["file"]["tmp_name"];
-
-        // Check if file already exists
-        if (file_exists($target_file)) {
-            unlink($target_file);
-        }
-
-        move_uploaded_file($temp_file, $target_file); 
     }
 
    function saveGenInfo( $cadet, $mysqli, $pPhone, $sPhone, $pEmail, $sEmail, $groupMe, $position, $major ) 
@@ -63,5 +49,35 @@ if ( !isset($_SESSION['login']) || !$_SESSION['login'] )
     }
 
     $cadet->updateCadet( $mysqli );
-    header('Location: myprofile.php');
+
+    if(isset($_FILES['file']))
+    {
+        $target_dir = "./assets/images/";        
+        $ext = ".jpg";
+        $target_file = $target_dir . $_SESSION['rin'] . $ext;
+        $temp_file = $_FILES["file"]["tmp_name"];
+        if ($_FILES["file"]["size"] > 500000) 
+        {
+            echo "<script type='text/javascript'>
+            alert('Sorry, your file is too large! Your file size is: " . $_FILES["file"]["size"] . " bytes and it needs to be < 500000 bytes (500kb)');
+            window.location.href='editprofile.php';
+            </script>";
+            $uploadOK = 0;
+        }
+        else
+        {
+            // Check if file already exists
+            if (file_exists($target_file)) 
+            {
+                unlink($target_file);
+            }
+
+            move_uploaded_file($temp_file, $target_file);  
+        }
+    }
+
+    if( $uploadOK == 1 )
+    {
+        header('Location: myprofile.php');
+    }
 ?>
